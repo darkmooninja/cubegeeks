cebaf4e4a15dc67420faca3357a871ece5e6c41b
darkmooninja <darkmooninja3481@gmail.com>
2025-02-27 17:57:59 -0800

    Update FlatSat_student.py

--- cubesat/FlatSat_student.py
+++ cubesat/FlatSat_student.py
@@ -15,240 +15,76 @@ You will need to complete the take_photo() function and configure the VARIABLES 
 
 
 #import libraries
+# Import libraries
 import time
 import board
-from PIL import Image
 from adafruit_lsm6ds.lsm6dsox import LSM6DSOX as LSM6DS
 from adafruit_lis3mdl import LIS3MDL
 from git import Repo
-from picamera2 import Picamera2, Preview
+from picamera2 import Picamera2
 
+# VARIABLES
+THRESHOLD = 8  # Acceleration threshold for detecting a shake
+REPO_PATH = "/home/cubegeeks/cubegeeks"  # Your GitHub repo path
+FOLDER_PATH = "/cubesat"  # Folder path for storing images
 
-#VARIABLES
-THRESHOLD = 8      #Any desired value from the accelerometer
-REPO_PATH = "/home/cubegeeks/cubegeeks"     #Your github repo path: ex. /home/pi/FlatSatChallenge
-FOLDER_PATH = "/cubesat"  #Your image folder path in your GitHub repo: ex. /Images
-
-#imu and camera initialization
+# IMU and camera initialization
 i2c = board.I2C()
 accel_gyro = LSM6DS(i2c)
 mag = LIS3MDL(i2c)
 picam2 = Picamera2()
 
-
 def git_push():
     """
-    This function is complete. Stages, commits, and pushes new images to your GitHub repo.
+    Stages, commits, and pushes new images to GitHub.
     """
     try:
         repo = Repo(REPO_PATH)
         origin = repo.remote('origin')
-        print('added remote')
         origin.pull()
-        print('pulled changes')
         repo.git.add(REPO_PATH + FOLDER_PATH)
         repo.index.commit('New Photo')
-        print('made the commit')
         origin.push()
-        print('pushed changes')
-    except:
-        print('Couldn\'t upload to git')
-
+        print('Photo uploaded to GitHub.')
+    except Exception as e:
+        print(f'Git upload failed: {e}')
 
 def img_gen(name):
     """
-    This function is complete. Generates a new image name.
-
-    Parameters:
-        name (str): your name ex. MasonM
+    Generates a unique image filename using the current timestamp.
     """
-    t = time.strftime("_%H%M%S")
-    imgname = (f'{REPO_PATH}/{FOLDER_PATH}/{name}{t}.jpg')
-    return imgname
-
+    timestamp = time.strftime("_%H%M%S")
+    return f'{REPO_PATH}/{FOLDER_PATH}/{name}{timestamp}.jpg'
 
 def take_photo():
     """
-    This function is NOT complete. Takes a photo when the FlatSat is shaken.
-    Replace psuedocode with your own code.
+    Monitors IMU data for shakes and takes a photo when a shake is detected.
     """
-
-
-    # while True:
-    #     accelx, accely, accelz = accel_gyro.acceleration
-    #     acceleration = accelx ** 2 + accely ** 2 + accelz ** 2
-    #     magnitude = acceleration ** (1/2)
+    while True:
+        accelx, accely, accelz = accel_gyro.acceleration
+        magnitude = (accelx ** 2 + accely ** 2 + accelz ** 2) ** 0.5
         
-
-    #     if magnitude > THRESHOLD:
-    #         print("SHAKE")
-    #         picam2.configure(picam2.create_preview_configuration({"format": "RGB888"}))
-    #         min_exp, max_exp, default_exp = picam2.camera_controls["AfPause"]
-
-    #         name = "Test"
-    #         photo_name = img_gen(name)
+        if magnitude > THRESHOLD:
+            print("SHAKE DETECTED! Capturing photo...")
             
-    #         picam2.start()
-
-    #         image = picam2.capture_image("main")
-    #         arr = picam2.capture_array("main")
-    #         image.save(photo_name)
-    #         git_push()
-    #         print("picture done")
-    #         picam2.stop()
-    #         return arr
-    picam2.configure(picam2.create_preview_configuration({"format": "RGB888"}))
-    min_exp, max_exp, default_exp = picam2.camera_controls["AfPause"]
-
-    name = "Test"
-    photo_name = img_gen(name)
-        
-    picam2.start()
-
-    image = picam2.capture_image("main")
-    # arr = picam2.capture_array("main")
-    image.save(photo_name)
-    git_push()
-    print("picture done")
-    picam2.stop()
-        # return arr
+            name = "Test"
+            photo_name = img_gen(name)
             
+            picam2.configure(picam2.create_still_configuration())
+            picam2.start()
+            time.sleep(1)  # Allow camera to adjust exposure
+            image = picam2.capture_image()
+            image.save(photo_name)
+            picam2.stop()
             
-
+            print(f"Photo saved: {photo_name}")
+            git_push()
             
-
-        
-
-        #CHECKS IF READINGS ARE ABOVE THRESHOLD
-            #PAUSE
-            #name = ""     #First Name, Last Initial  ex. MasonM
-            #TAKE PHOTO
-            #PUSH PHOTO TO GITHUB
-        
-        #PAUSE
-def process_image(image):
-    d = dict()
-    d["Area1"] = 0
-    d["Area2"] = 0
-    d["Area3"] = 0
-    d["Area4"] = 0
-
-    #White: (256, 256, 256), #black: (0, 0, 0)
-    for i in range(len(image)):
-        for j in range(len(image[0])):
-            #area 1
-            temp = sum(image[i][j])
-            if i <= 239:
-                if j <= 319:
-                    #Area1
-                    if temp >= 175:
-                        d["Area1"] += 1
-                else:
-                    #Area3
-                    if temp >= 175:
-                        d["Area3"] += 1
-            else:
-                if j <= 319:
-                    #Area2
-                    if temp >= 175:
-                        d["Area2"] += 1
-                else:
-                    #Area4
-                    if temp >= 175:
-                        d["Area4"] += 1    
-    return d
-def detect_difference(before, after):
-    arr = [False] * 4
-    for i in range(len(before)):
-        if before[i] - after[i] > 10000:
-            arr[i] = True
-
-    return arr
-
-def detect_difference_one(Onephoto):
-    arr = [False] * 4
-    for i in range(len(Onephoto)):
-        if Onephoto[i] > 10000:
-            arr[i] = True
-    return arr
-
-
-def compare(before, after):
-
-    d = dict()
-    d["Area1"] = 0
-    d["Area2"] = 0
-    d["Area3"] = 0
-    d["Area4"] = 0
-
-    for i in range(len(before)):
-        for j in range(len(before[0])):
-
-            cell_before = sum(before[i][j])
-            cell_after = sum(after[i][j])
-
-            if i <= 239:
-                if j <= 319:
-                    #Area1
-                    if cell_before - cell_after >= 150:
-                        d["Area1"] += 1
-                else:
-                    #Area3
-                    if cell_before - cell_after >= 150:
-                        d["Area3"] += 1
-            else:
-                if j <= 319:
-                    #Area2
-                    if cell_before - cell_after >= 150:
-                        d["Area2"] += 1
-                else:
-                    #Area4
-                    if cell_before - cell_after >= 150:
-                        d["Area4"] += 1
-    
-    lst = list(d.values())
-    result = [False] * 4
-    for i in range(len(lst)):
-        if lst[i] >= 15000:
-            result[i] = True
-    return result
+            time.sleep(2)  # Prevent multiple triggers in quick succession
 
 def main():
-    # Stop = 0
-    # while True:
-    #     if Stop == 3:
-    #         break
-    #     image_1 = take_photo()
+    print("IMU Shake Detection Running...")
+    take_photo()
 
-    #     processed_1 = process_image(image_1)
-
-    #     print(processed_1)
-
-    #     arr_1 = list(processed_1.values())
-
-    #     image_1_black = detect_difference_one(arr_1)
-
-    #     print(image_1_black)
-
-    #     Stop+=1
-    #     time.sleep(1)
-    image_1 = take_photo() #taking a picture TEST
-
-        
-    # image_2 = take_photo()
-
-    # processed_2 = process_image(image_2)
-
-    # print(processed_2)
-
-    # arr_2 = list(processed_2.values())
-
-    # image_2_black = detect_difference_one(arr_2)
-
-    # print("Image 1: " + image_1_black)
-
-    
-
-if __name__ == '__main__':
-    print("Hello World!")
+if __name__ == "__main__":
     main()
